059e8f0084cf48ad58805302af99ce92171f1360
Wiped-outt <rphlthrr@gmail.com>
2023-10-19 13:41:50 -0300

    a

--- index.php
+++ index.php
@@ -19,14 +19,20 @@
         <div id="posts-container">
         <?php foreach($posts as $post): ?>
             <div class="post-box">
-                <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
-                <h2 class="post-title"><a href="<?= $BASE_URL ?>post.php?id=<?= $post-"></a></h2>
+            <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+            <h2 class="post-title"><a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"></a></h2>
+            <p class="post-description"><?= $post['description'] ?></p>
+            <div class="tags-container">
+                <?php foreach($post['tags'] as $tag): ?>
+                <a href="#"><?= $tag ?></a>
+                <?php endforeach; ?>
             </div>
-
+            </div>
+        <?php endforeach; ?>
         </div>
-
     </main>
 
 <?php
+    //Inclusão do footer
     include_once("templates/footer.php");
 ?>
