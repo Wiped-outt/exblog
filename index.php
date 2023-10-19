82921413b6889952b42972ba5d142cb9e952030a
Wiped-outt <rphlthrr@gmail.com>
2023-10-19 13:27:04 -0300

    a

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
+    // Inclusão do header
     include_once("templates/header.php");
 ?>
 
-<h1>Olá mundo!! Bem-vindo ao PHP!!</h1>
+<!-- <h1>Olá mundo!</h1> -->
+
+    <!-- // pata teste
+    var_dump($posts);
+    echo "<br><br>";
+    print_r($posts); -->
+    
+    <main>
+
+        <div id="title-container">
+            <h1>Code Blog</h1>
+            <p>O seu blog de Programação</p>
+        </div>
+        <div id="posts-container">
+        <?php foreach($posts as $post): ?>
+            <div class="post-box">
+                <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+                <h2 class="post-title"><a href="<?= $BASE_URL ?>post.php?id=<?= $post-"></a></h2>
+            </div>
+
+        </div>
+
+    </main>
 
 <?php
     include_once("templates/footer.php");
--- template/header.php
+++ template/header.php
@@ -1,27 +1,31 @@
-<?php
+<html>
+    <head>
+        <?php
 
-require_once ("herpers/url.php");
+            require_once("helper/url.php");
+            require_once("data/post.php");
+            require_once("data/categorias.php");
+            ?>
 
-?>
+            <link rel="stylesheet" href="<?=$BASE_URL ?>/CSS/style.css">
 
-<link rel="stylesheet" href="<?=$BASE_URL ?>/CSS/style.css">
+            <!-- Fontes do Google -->
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 
-<!-- Inserir font -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
-
-</head>
-<body>
-    <header>
-        <a href="<?=$BASE_URL ?>"id="logo">
-        <img src="<?=$BASE_URL ?>/img/logo.svg" alt="Logo Blog">
-        </a>
-        <ul id="navbar">
-            <li><a href="<?=$BASE_URL ?>"class="nav-link">Home</a></li>
-            <li><a href="#" class="nav-link">Categorias</a></li>
-            <li><a href="#" class="nav-link">Sobre</a></li>
-            <li><a href="<?=$BASE_URL ?>/contato.php"class="nav-link">Contato</a></li>
-        </ul>
-    </header>
-</body>
+            </head>
+            <body>
+                <header>
+                    <a href="<?=$BASE_URL ?>"id="logo">
+                    <img src="<?=$BASE_URL ?>/img/logo.svg" alt="Logo Blog">
+                    </a>
+                    <ul id="navbar">
+                        <li><a href="<?=$BASE_URL ?>"class="nav-link">Home</a></li>
+                        <li><a href="#" class="nav-link">Categorias</a></li>
+                        <li><a href="#" class="nav-link">Sobre</a></li>
+                        <li><a href="<?=$BASE_URL ?>/contato.php"class="nav-link">Contato</a></li>
+                    </ul>
+                </header>
+            </body>
+</html>
